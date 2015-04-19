fc2ab37e8ab0a23c28865bc0dc3002f5b26e6d70
simaQ <sima.zhang1990@gmail.com>
2015-04-19 16:37:28 +0800

    correct code style

--- examples/simple.js
+++ examples/simple.js
@@ -3,6 +3,6 @@
 require('rc-slider/assets/index.css');
 var Slider = require('rc-slider');
 var React = require('react');
-// React.render(<Slider marks={["一","二","三","四","五"]} index={3}/>, document.getElementById('__react-content'));
+// React.render(<Sliders marks={["一","二","三","四","五"]} index={3}/>, document.getElementById('__react-content'));
 // React.render(<Slider className='rc-slider' step={20}/>, document.getElementById('__react-content'));
 React.render(<Slider />, document.getElementById('__react-content'));
