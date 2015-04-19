612d0660ed56f3465ac6725bdf0ed343dd962b45
simaQ <sima.zhang1990@gmail.com>
2015-04-19 16:25:56 +0800

    version 1.0.0

---
+++ examples/simple.js
@@ -0,0 +1,8 @@
+/** @jsx React.DOM */
+// use jsx to render html, do not modify simple.html
+require('rc-slider/assets/index.css');
+var Slider = require('rc-slider');
+var React = require('react');
+// React.render(<Slider marks={["一","二","三","四","五"]} index={3}/>, document.getElementById('__react-content'));
+// React.render(<Slider className='rc-slider' step={20}/>, document.getElementById('__react-content'));
+React.render(<Slider />, document.getElementById('__react-content'));
