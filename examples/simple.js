76ba795e0d4be5543d0e1233be328b3eb5c1a969
yiminghe <yiminghe@gmail.com>
2015-04-22 23:40:43 +0800

    update examples at 1429717243780

--- examples/simple.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-// use jsx to render html, do not modify simple.html
-require('rc-slider/assets/index.css');
-var Slider = require('rc-slider');
-var React = require('react');
-// React.render(<Sliders marks={["一","二","三","四","五"]} index={3}/>, document.getElementById('__react-content'));
-// React.render(<Slider className='rc-slider' step={20}/>, document.getElementById('__react-content'));
-React.render(<Slider />, document.getElementById('__react-content'));
