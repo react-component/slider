d6779510f916cce97dd4fb7d332aa6914a17bd1c
SimaQ <sima.zhang1990@gmail.com>
2015-10-15 10:45:40 +0800

    test: update test case.

--- examples/simple.js
+++ examples/simple.js
@@ -10,5 +10,6 @@ function onChange(v){
 }
 
 // React.render(<div style={{width:400,margin:100}}><Slider marks={["一","二","三","四","五"]} defaultIndex={2} /></div>, document.getElementById('__react-content'));
+// React.render(<div style={{width:400,margin:100}}><Slider withDots className='rc-slider' step={20}/></div>, document.getElementById('__react-content'));
 // React.render(<div style={{width:400,margin:100}}><Slider className='rc-slider' step={20}/></div>, document.getElementById('__react-content'));
 React.render(<div style={{width:400,margin:100}}><Slider onChange={onChange}  tipTransitionName='rc-slider-tooltip-zoom-down'/></div>, document.getElementById('__react-content'));
