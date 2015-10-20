a1dd641e70deed841effb368b90b7e40e9f87e76
SimaQ <sima.zhang1990@gmail.com>
2015-10-20 19:08:08 +0800

    update demo.

--- examples/simple.js
+++ examples/simple.js
@@ -23,5 +23,13 @@ React.render(
     <div style={{width:400,margin:100}}>
       <Slider step={20}/>
     </div>
+    <div style={{width:400,margin:100}}>
+      <p>包含关系</p>
+      <Slider marks={["状态1","状态2","状态3","状态4"]} defaultIndex={1} />
+    </div>
+    <div style={{width:400,margin:100}}>
+      <p>并列关系</p>
+      <Slider marks={["状态1","状态2","状态3","状态4"]} isIncluded={false} defaultIndex={1} />
+    </div>
   </div>
   , document.getElementById('__react-content'));
