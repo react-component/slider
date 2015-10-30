4b854b04e573f7c272827d466606511c5557648f
SimaQ <sima.zhang1990@gmail.com>
2015-10-30 12:13:12 +0800

    doc: update demo.

--- examples/simple.js
+++ examples/simple.js
@@ -4,7 +4,7 @@ require('rc-slider/assets/index.less');
 
 var React = require('react');
 var Slider = require('rc-slider');
-var style = {width:400,margin:100};
+var style = {width:400,margin:50};
 
 function onChange(v) {
   console.log(v);
@@ -13,35 +13,31 @@ function onChange(v) {
 React.render(
   <div>
     <div style={style}>
+      <p>基础滑块</p>
       <Slider onChange={onChange} tipTransitionName='rc-slider-tooltip-zoom-down' />
     </div>
     <div style={style}>
-      <Slider marks={["一","二","三","四","五"]} defaultIndex={2} />
-    </div>
-    <div style={style}>
-      <Slider withDots step={20} />
-    </div>
-    <div style={style}>
+      <p>基础滑块，step=20</p>
       <Slider step={20} />
     </div>
     <div style={style}>
+      <p>基础滑块，step=20 带原点</p>
+      <Slider withDots step={20} />
+    </div>
+    <div style={style}>
+      <p>双滑块</p>
       <Slider range={true} min={10} max={90} values={[0, 30]} onChange={onChange} />
     </div>
     <div style={style}>
+      <p>双滑块，step=20 </p>
       <Slider range={true} withDots step={20} values={[0, 30]} onChange={onChange} isIncluded={false} />
     </div>
     <div style={style}>
-      <Slider range={true} withDots step={20} values={[0, 0]} onChange={onChange} isIncluded={false} />
-    </div>
-    <div style={style}>
-      <Slider range={true} withDots step={20} values={[100, 100]} onChange={onChange} isIncluded={false} />
-    </div>
-    <div style={style}>
-      <p>包含关系</p>
+      <p>分段式滑块（包含关系）</p>
       <Slider marks={["状态1","状态2","状态3","状态4"]} defaultIndex={1} />
     </div>
     <div style={style}>
-      <p>并列关系</p>
+      <p>分段式滑块（并列关系）</p>
       <Slider marks={["状态1","状态2","状态3","状态4"]} isIncluded={false} defaultIndex={1} />
     </div>
   </div>
