d3742ba8d37905b708e29aa86c64d4ed27b85ce7
Lion <maolion.j@gmail.com>
2016-03-17 20:09:08 +0800

    fix marks only one bug

--- src/Marks.jsx
+++ src/Marks.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 const Marks = ({className, marks, included, upperBound, lowerBound, max, min}) => {
   const marksKeys = Object.keys(marks);
   const marksCount = marksKeys.length;
-  const unit = 100 / (marksCount - 1);
+  const unit = 100 / ((marksCount - 1)||1);
   const markWidth = unit * 0.9;
 
   const range = max - min;
