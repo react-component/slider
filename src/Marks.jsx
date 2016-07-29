6acb1fccb9ffce952dcbe79b10cd79b8633f2b5f
Tomi Saarinen <tomi.saarinen@rohea.com>
2016-07-29 18:01:15 +0300

    add point as classname for marks

--- src/Marks.jsx
+++ src/Marks.jsx
@@ -14,6 +14,7 @@ const Marks = ({className, vertical, marks, included, upperBound, lowerBound, ma
     const markClassName = classNames({
       [className + '-text']: true,
       [className + '-text-active']: isActived,
+      [className + '-text-point-' + point]: true, 
     });
 
     const bottomStyle = {
