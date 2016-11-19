92998641858fd0bcd87be2dbc45b4485cd80e941
Gunavel <sgb.rated@gmail.com>
2016-11-19 20:17:44 +0530

    cleared lint errors

--- examples/slider.js
+++ examples/slider.js
@@ -85,7 +85,8 @@ ReactDOM.render(
     </div>
     <div style={style}>
       <p>Basic Slider with ToolTip visible always</p>
-      <Slider tipTransitionName="rc-slider-tooltip-zoom-down" onChange={log} toolTipVisibleAlways={true} />
+      <Slider tipTransitionName="rc-slider-tooltip-zoom-down" onChange={log} toolTipVisibleAlways
+      />
     </div>
     <div style={style}>
       <p>Basic Slider，`step=20`</p>
