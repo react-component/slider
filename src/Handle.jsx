5e41a1dfe6b25faf9aa324fa3687fdb30e40be6f
Maksim Lipouski <maksim.lipouski@gmail.com>
2016-11-30 19:00:26 +0300

    feat: added tipAlwaysVisible prop

--- src/Handle.jsx
+++ src/Handle.jsx
@@ -28,6 +28,7 @@ export default class Handle extends React.Component {
       tooltipPrefixCls,
       className,
       tipTransitionName,
+      tipAlwaysVisible,
       tipFormatter,
       vertical,
       offset,
@@ -37,19 +38,30 @@ export default class Handle extends React.Component {
     } = this.props;
 
     const style = vertical ? { bottom: `${offset}%` } : { left: `${offset}%` };
+
+    let handleProps = {
+      className,
+      style,
+    };
+
+    if (!tipAlwaysVisible) {
+      handleProps = {
+        ...handleProps,
+        onMouseUp: this.showTooltip.bind(this),
+        onMouseEnter: this.showTooltip.bind(this),
+        onMouseLeave: this.hideTooltip.bind(this),
+      };
+    }
+
     const handle = (
-      <div className={className} style={style}
-        onMouseUp={this.showTooltip.bind(this)}
-        onMouseEnter={this.showTooltip.bind(this)}
-        onMouseLeave={this.hideTooltip.bind(this)}
-      />
+      <div {...handleProps} />
     );
 
     if (noTip) {
       return handle;
     }
 
-    const isTooltipVisible = dragging || this.state.isTooltipVisible;
+    const isTooltipVisible = tipAlwaysVisible || dragging || this.state.isTooltipVisible;
     return (
       <Tooltip
         prefixCls={tooltipPrefixCls || `${prefixCls}-tooltip`}
@@ -72,6 +84,7 @@ Handle.propTypes = {
   vertical: React.PropTypes.bool,
   offset: React.PropTypes.number,
   tipTransitionName: React.PropTypes.string,
+  tipAlwaysVisible: React.PropTypes.bool,
   tipFormatter: React.PropTypes.func,
   value: React.PropTypes.number,
   dragging: React.PropTypes.bool,
--- src/Slider.jsx
+++ src/Slider.jsx
@@ -452,6 +452,7 @@ class Slider extends React.Component {
         marks,
         max, min,
         tipTransitionName,
+        tipAlwaysVisible,
         tipFormatter,
         children,
     } = this.props;
@@ -476,6 +477,7 @@ class Slider extends React.Component {
       tooltipPrefixCls,
       noTip: isNoTip,
       tipTransitionName,
+      tipAlwaysVisible,
       tipFormatter,
       vertical,
     };
@@ -560,6 +562,7 @@ Slider.propTypes = {
   onAfterChange: React.PropTypes.func,
   handle: React.PropTypes.element,
   tipTransitionName: React.PropTypes.string,
+  tipAlwaysVisible: React.PropTypes.bool,
   tipFormatter: React.PropTypes.func,
   dots: React.PropTypes.bool,
   range: React.PropTypes.oneOfType([
@@ -578,6 +581,7 @@ Slider.defaultProps = {
   prefixCls: 'rc-slider',
   className: '',
   tipTransitionName: '',
+  tipAlwaysVisible: false,
   min: 0,
   max: 100,
   step: 1,
