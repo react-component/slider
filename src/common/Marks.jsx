87862d8928a052354ecf35701c5bc2437b45172b
Jeremy Nagel <jnagel@cozero.com.au>
2017-08-31 12:56:11 +1200

    Properly calculate width of mark text

--- src/common/Marks.jsx
+++ src/common/Marks.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
 
+const calculatedWidths = {};
+
+function getWidthOfText(text) {
+  const textToCheck = typeof text !== 'string' ? text.props.children : text;
+  if (calculatedWidths[textToCheck]) {
+    return calculatedWidths[textToCheck];
+  }
+  const element = document.createElement('canvas');
+  const context = element.getContext('2d');
+  const width = context.measureText(textToCheck).width;
+  calculatedWidths[textToCheck] = width;
+  return width;
+}
+
 const Marks = ({
   className,
   vertical,
@@ -11,7 +25,6 @@ const Marks = ({
   max, min,
 }) => {
   const marksKeys = Object.keys(marks);
-
   const range = max - min;
   const elements = marksKeys.map(parseFloat).sort((a, b) => a - b).map(point => {
     const isActive = (!included && point === upperBound) ||
@@ -21,22 +34,27 @@ const Marks = ({
       [`${className}-text-active`]: isActive,
     });
 
-    const bottomStyle = {
-      bottom: `${(point - min) / range * 100}%`,
-    };
-
-    const leftStyle = {
-      left: `${(point - min) / range * 100}%`,
-    };
-
-    const style = vertical ? bottomStyle : leftStyle;
-
     const markPoint = marks[point];
     const markPointIsObject = typeof markPoint === 'object' &&
             !React.isValidElement(markPoint);
     const markLabel = markPointIsObject ? markPoint.label : markPoint;
+    const markWidth = getWidthOfText(markLabel);
+
+    const bottomStyle = {
+      marginBottom: '-50%',
+      bottom: `${(point - min) / range * 100}%`,
+    };
+
+    const leftStyle = {
+      width: `${markWidth}px`,
+      marginLeft: `${-markWidth / 2}px`,
+      left: `${(point - min) / range * 100}%`,
+    };
+
+    const style = vertical ? bottomStyle : leftStyle;
     const markStyle = markPointIsObject ?
             { ...style, ...markPoint.style } : style;
+
     return (
       <span
         className={markClassName}
