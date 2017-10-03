6f1867ba10e81ec9df8a2daf0749566a75716b20
polkpolio1 <polkpolio1@gmail.com>
2017-10-03 13:26:50 +0300

    Refactoring. Take out ownerDocument to this.document

--- src/common/createSlider.jsx
+++ src/common/createSlider.jsx
@@ -86,6 +86,10 @@ export default function createSlider(Component) {
       this.removeDocumentEvents();
     }
 
+    componentDidMount() {
+      this.document = this.sliderRef.ownerDocument;
+    }
+
     onMouseDown = (e) => {
       if (e.button !== 0) { return; }
 
@@ -138,13 +142,13 @@ export default function createSlider(Component) {
 
     addDocumentTouchEvents() {
       // just work for Chrome iOS Safari and Android Browser
-      this.onTouchMoveListener = addEventListener(this.sliderRef.ownerDocument, 'touchmove', this.onTouchMove);
-      this.onTouchUpListener = addEventListener(this.sliderRef.ownerDocument, 'touchend', this.onEnd);
+      this.onTouchMoveListener = addEventListener(this.document, 'touchmove', this.onTouchMove);
+      this.onTouchUpListener = addEventListener(this.document, 'touchend', this.onEnd);
     }
 
     addDocumentMouseEvents() {
-      this.onMouseMoveListener = addEventListener(this.sliderRef.ownerDocument, 'mousemove', this.onMouseMove);
-      this.onMouseUpListener = addEventListener(this.sliderRef.ownerDocument, 'mouseup', this.onEnd);
+      this.onMouseMoveListener = addEventListener(this.document, 'mousemove', this.onMouseMove);
+      this.onMouseUpListener = addEventListener(this.document, 'mouseup', this.onEnd);
     }
 
     removeDocumentEvents() {
