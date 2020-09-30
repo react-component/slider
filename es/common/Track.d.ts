7b10f1e1d4181313251989a453613797cb52c839
César Pinheiro <cesar.pinheiro@kodly.io>
2020-09-30 17:03:51 +0100

    Rollback to previous version

--- es/common/Track.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-declare const Track: (props: any) => JSX.Element;
-export default Track;
--- es/common/createSlider.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { GenericSliderProps, GenericSliderState, GenericSlider } from '../interface';
-export default function createSlider<Props extends GenericSliderProps, State extends GenericSliderState>(Component: GenericSlider<Props, State>): React.ComponentClass<Props, State>;
