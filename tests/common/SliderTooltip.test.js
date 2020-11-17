69dc59270ca46ae2d3c4b5aa073d2bc75dfc5b16
afc163 <afc163@gmail.com>
2020-11-17 19:17:19 +0800

    fix forcePopupAlign null

--- tests/common/SliderTooltip.test.js
+++ tests/common/SliderTooltip.test.js
@@ -3,22 +3,37 @@ import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 import SliderTooltip from '../../src/common/SliderTooltip';
 
-it('should keepAlign by calling forcePopupAlign', async () => {
-  let ref;
-  mount(
-    <SliderTooltip
-      title="30"
-      visible
-      ref={node => {
-        ref = node;
-      }}
-    >
-      <span>aaaa</span>
-    </SliderTooltip>,
-  );
-  ref.forcePopupAlign = jest.fn();
-  await act(async () => {
-    await new Promise(res => setTimeout(res, 200));
+describe('SliderTooltip', () => {
+  it('should keepAlign by calling forcePopupAlign', async () => {
+    let ref;
+    mount(
+      <SliderTooltip
+        title="30"
+        visible
+        ref={node => {
+          ref = node;
+        }}
+      >
+        <span>aaaa</span>
+      </SliderTooltip>,
+    );
+    ref.forcePopupAlign = jest.fn();
+    await act(async () => {
+      await new Promise(res => setTimeout(res, 200));
+    });
+    expect(ref.forcePopupAlign).toHaveBeenCalled();
   });
-  expect(ref.forcePopupAlign).toHaveBeenCalled();
-});
+
+  it('should not crash when unmount', async () => {
+    const wrapper = mount(
+      <SliderTooltip
+        title="30"
+        visible
+      >
+        <span>aaaa</span>
+      </SliderTooltip>,
+    );
+    wrapper.unmount();
+  });
+})
+
