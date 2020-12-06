b7e528fe145aabcc6e24141da784d963b77a55dd
xrkffgg <xrkffgg@vip.qq.com>
2020-12-06 20:37:11 +0800

    chore: fix readme

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,10 @@
 // more config: https://d.umijs.org/config
 import { defineConfig } from 'dumi';
 
+const name = 'rc-slider';
+
 export default defineConfig({
-  title: 'rc-slider',
+  title: name,
   favicon:
     'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   logo:
@@ -13,5 +15,18 @@ export default defineConfig({
     `a img + svg {
       display: none;
     }`
-  ]
+  ],
+  scripts: [{
+    content: `
+      (function () {
+        var timer = setInterval(function() {
+          try {
+            var menuList = document.getElementsByClassName('__dumi-default-menu-list');
+            menuList[0].childNodes[0].childNodes[0].innerText = '${name}';
+            clearInterval(timer);
+          } catch (e) {}
+        }, 200)
+      })();
+    `
+  }]
 });
