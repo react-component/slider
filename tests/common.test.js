f289a3889a0305690878da2cfbfbdf348b2c58b5
Yann Combarnous <yann.combarnous@cbre.com>
2022-04-29 10:21:19 +0200

    fix: prettier

--- tests/common.test.js
+++ tests/common.test.js
@@ -52,13 +52,25 @@ describe('Common', () => {
   });
 
   it('should render dots correctly when dotStyle is dynamic`', () => {
-    const { container: container1 } = render(<Slider value={50} step={10} dots dotStyle={dotValue => ({width: dotValue})}/>);
-    expect(container1.getElementsByClassName('rc-slider-dot')[1]).toHaveStyle('left: 9.09091%; transform: translateX(50%); width: 10px');
-    expect(container1.getElementsByClassName('rc-slider-dot')[2]).toHaveStyle('left: 18.18182%; transform: translateX(50%); width: 20px');
+    const { container: container1 } = render(
+      <Slider value={50} step={10} dots dotStyle={dotValue => ({width: `${dotValue}px`})}/>
+    );
+    expect(
+      container1.getElementsByClassName('rc-slider-dot')[1]
+    ).toHaveStyle('left: 9.09091%; transform: translateX(50%); width: 10px');
+    expect(
+      container1.getElementsByClassName('rc-slider-dot')[2]
+    ).toHaveStyle('left: 18.18182%; transform: translateX(50%); width: 20px');
 
-    const { container: container2 } = render(<Slider range value={[20, 50]} step={10} dots activeDotStyle={dotValue => ({width: dotValue})}/>);
-    expect(container2.getElementsByClassName('rc-slider-dot-active')[1]).toHaveStyle('left: 36.36364%; transform: translateX(50%); width: 30px');
-    expect(container2.getElementsByClassName('rc-slider-dot-active')[2]).toHaveStyle('left: 45.54455%; transform: translateX(50%); width: 40px');
+    const { container: container2 } = render(
+      <Slider range value={[20, 50]} step={10} dots activeDotStyle={dotValue => ({width: `${dotValue}px`})}/>
+    );
+    expect(
+      container2.getElementsByClassName('rc-slider-dot-active')[1]
+    ).toHaveStyle('left: 36.36364%; transform: translateX(50%); width: 30px');
+    expect(
+      container2.getElementsByClassName('rc-slider-dot-active')[2]
+    ).toHaveStyle('left: 45.54455%; transform: translateX(50%); width: 40px');
   });
 
   it('should not set value greater than `max` or smaller `min`', () => {
