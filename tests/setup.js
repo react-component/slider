f1a6c28a1229e75e3fa3446bbeb3e51186466481
二货机器人 <smith3816@gmail.com>
2023-09-21 10:48:27 +0800

    chore: update dumi

--- tests/setup.js
+++
@@ -1,9 +0,0 @@
-global.requestAnimationFrame = global.requestAnimationFrame || function _raf(cb) {
-  return setTimeout(cb, 0);
-};
-require('regenerator-runtime');
-
-const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-Enzyme.configure({ adapter: new Adapter() });
