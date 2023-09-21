fa4faadf99fbb65dad03a621bc27d3032f5f47cb
二货机器人 <smith3816@gmail.com>
2023-09-21 13:44:40 +0800

    chore: styles & classNames

--- src/Tracks/index.tsx
+++ src/Tracks/index.tsx
@@ -1,8 +1,9 @@
+import cls from 'classnames';
 import * as React from 'react';
 import SliderContext from '../context';
-import Track from './Track';
 import type { OnStartMove } from '../interface';
 import { getIndex } from '../util';
+import Track from './Track';
 
 export interface TrackProps {
   prefixCls: string;
@@ -14,8 +15,9 @@ export interface TrackProps {
 
 export default function Tracks(props: TrackProps) {
   const { prefixCls, style, values, startPoint, onStartMove } = props;
-  const { included, range, min } = React.useContext(SliderContext);
+  const { included, range, min, styles, classNames } = React.useContext(SliderContext);
 
+  // =========================== List ===========================
   const trackList = React.useMemo(() => {
     if (!range) {
       // null value do not have track
@@ -35,7 +37,7 @@ export default function Tracks(props: TrackProps) {
     }
 
     // Multiple
-    const list = [];
+    const list: { start: number; end: number }[] = [];
 
     for (let i = 0; i < values.length - 1; i += 1) {
       list.push({
@@ -47,17 +49,39 @@ export default function Tracks(props: TrackProps) {
     return list;
   }, [values, range, startPoint, min]);
 
-  return (included
-    ? trackList.map(({ start, end }, index) => (
+  // ========================== Render ==========================
+  let tracksNode: React.ReactElement = null;
+
+  if (classNames.tracks || styles.tracks) {
+    tracksNode = (
+      <Track
+        index={null}
+        prefixCls={prefixCls}
+        start={trackList[0].start}
+        end={trackList[trackList.length - 1].end}
+        replaceCls={cls(classNames.tracks, `${prefixCls}-tracks`)}
+        style={styles.tracks}
+      />
+    );
+  }
+
+  return (included ? (
+    <>
+      {tracksNode}
+      {trackList.map(({ start, end }, index) => (
         <Track
           index={index}
           prefixCls={prefixCls}
-          style={getIndex(style, index)}
+          style={{
+            ...getIndex(style, index),
+            ...styles.track,
+          }}
           start={start}
           end={end}
           key={index}
           onStartMove={onStartMove}
         />
-      ))
-    : null) as unknown as React.ReactElement;
+      ))}
+    </>
+  ) : null) as unknown as React.ReactElement;
 }
--- src/interface.ts
+++ src/interface.ts
@@ -5,3 +5,9 @@ export type Direction = 'rtl' | 'ltr' | 'ttb' | 'btt';
 export type OnStartMove = (e: React.MouseEvent | React.TouchEvent, valueIndex: number) => void;
 
 export type AriaValueFormat = (value: number) => string;
+
+export type SemanticName = 'tracks' | 'track' | 'rail' | 'handle';
+
+export type SliderClassNames = Partial<Record<SemanticName, string>>;
+
+export type SliderStyles = Partial<Record<SemanticName, React.CSSProperties>>;
