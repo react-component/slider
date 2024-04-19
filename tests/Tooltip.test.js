f33020e0fa26557e3ba74909e1aea2e717198bbc
二货机器人 <smith3816@gmail.com>
2024-04-19 14:08:10 +0800

    test: coverage

---
+++ tests/Tooltip.test.js
@@ -0,0 +1,28 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import Slider from '../src/Slider';
+
+describe('Slider.Tooltip', () => {
+  it('internal activeHandleRender support', () => {
+    const { container } = render(
+      <Slider
+        range
+        defaultValue={[20, 50]}
+        activeHandleRender={(node, info) =>
+          React.cloneElement(node, {
+            'data-test': 'bamboo',
+            'data-value': info.value,
+          })
+        }
+      />,
+    );
+    expect(container.querySelector('.rc-slider-handle[data-test]')).toBeTruthy();
+
+    // Click second
+    fireEvent.mouseEnter(container.querySelectorAll('.rc-slider-handle')[1]);
+    expect(
+      container.querySelector('.rc-slider-handle[data-value]').getAttribute('data-value'),
+    ).toBe('50');
+  });
+});
