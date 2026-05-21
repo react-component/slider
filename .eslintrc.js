b1c50a3d2ce614c9711168012e988371b7744bf4
高艳兵 <yanbing.gao@aliyun.com>
2026-05-21 14:42:38 +0800

    chore: address eslint review comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 1,
     '@typescript-eslint/no-empty-interface': 1,
     '@typescript-eslint/no-inferrable-types': 0,
-    '@typescript-eslint/switch-exhaustiveness-check': 0,
     'react/no-find-dom-node': 1,
     'react/require-default-props': 0,
     'no-confusing-arrow': 0,
